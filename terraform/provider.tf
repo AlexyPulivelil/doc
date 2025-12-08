1d831fdb91bb7a565174782ec40e73789b5e8055
alexypuli <alexy.thomas@prevalent.ai>
2025-12-08 20:49:25 +0530

    adding terrafom

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,11 @@
 terraform {
+  backend "s3" {
+    bucket = "doc-api-terraform-state"
+    key    = "doc-api/terraform.tfstate"
+    region = "ap-south-1"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
